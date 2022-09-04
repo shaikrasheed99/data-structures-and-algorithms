f4cff5fb4a8440bf9f6119b7b1d1c8fb287ce5c5
Rasheed <shaikrasheed3014@gmail.com>
2022-09-04 23:21:39 +0530

    Patterns: Example of Square pattern

---
+++ src/patterns/PatternSixteen.java
@@ -0,0 +1,36 @@
+package patterns;
+
+/**
+ * Print below pattern
+ *      #####
+ *      #   #
+ *      #   #
+ *      #   #
+ *      #####
+ */
+
+public class PatternSixteen {
+    public static void main(String[] args) {
+        int n = 5;
+        printPattern(n);
+    }
+
+    private static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i == 1 || i == n) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print("#");
+                }
+            } else {
+                for (int j = 1; j <= n; j++) {
+                    if (j == 1 || j == n) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
