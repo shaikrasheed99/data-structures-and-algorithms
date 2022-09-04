b818da41b45385b278101798305c765922729faf
Rasheed <shaikrasheed3014@gmail.com>
2022-09-04 20:12:58 +0530

    Patterns: Example of Numerical Star pattern 1

---
+++ src/patterns/PatternThree.java
@@ -0,0 +1,25 @@
+package patterns;
+
+/**
+ * Print below pattern
+ * 1
+ * 1 2
+ * 1 2 3
+ * 1 2 3 4
+ */
+
+public class PatternThree {
+    public static void main(String[] args) {
+        int n = 5;
+        printPattern(n);
+    }
+
+    private static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
